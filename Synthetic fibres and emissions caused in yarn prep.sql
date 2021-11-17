1e8296d1da08fd719a09dd7abe36e27bf833df1b
Tolulope <omosefunmiblessing@gmail.com>
2021-11-17 09:42:19 -0700

    Add files via upload
    Analysing emissions from various Materials .

---
+++ Synthetic fibres and emissions caused in yarn prep.sql
@@ -0,0 +1,48 @@
+CREATE TABLE mytable(
+   Material     VARCHAR(9) NOT NULL PRIMARY KEY
+  ,Power_Source VARCHAR(27) NOT NULL
+  ,CO2emBTU     NUMERIC(5,1) NOT NULL
+  ,C02e1MJkg    NUMERIC(11,4) NOT NULL
+  ,Emissionskg  NUMERIC(11,3) NOT NULL
+  ,EnergyMJKg   INTEGER  NOT NULL
+);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Nylon','Coal (anthracite)',228.6,531689.9958,69119699.45,130);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Nylon','Coal (bituminous)',205.7,478427.9621,62195635.07,130);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Nylon','Coal (lignite)',215.4,500988.7362,65128535.71,130);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Nylon','Coal (subbituminous)',214.3,498430.2979,64795938.73,130);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Nylon','Diesel fuel and heating oil',161.3,375160.0889,48770811.56,130);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Nylon','Gasoline (without ethanol)',157.2,365624.0916,47531131.91,130);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Nylon','Propane',139,323293.567,42028163.71,130);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Nylon','Natural gas',117,272124.801,35376224.13,130);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Polyester','Coal (anthracite)',228.6,531689.9958,66461249.48,125);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Polyester','Coal (bituminous)',205.7,478427.9621,59803495.26,125);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Polyester','Coal (lignite)',215.4,500988.7362,62623592.03,125);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Polyester','Coal (subbituminous)',214.3,498430.2979,62303787.24,125);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Polyester','Diesel fuel and heating oil',161.3,375160.0889,46895011.11,125);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Polyester','Gasoline (without ethanol)',157.2,365624.0916,45703011.45,125);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Polyester','Propane',139,323293.567,40411695.88,125);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Polyester','Natural gas',117,272124.801,34015600.13,125);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Acrylic','Coal (anthracite)',228.6,531689.9958,93045749.27,175);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Acrylic','Coal (bituminous)',205.7,478427.9621,83724893.37,175);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Acrylic','Coal (lignite)',215.4,500988.7362,87673028.84,175);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Acrylic','Coal (subbituminous)',214.3,498430.2979,87225302.13,175);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Acrylic','Diesel fuel and heating oil',161.3,375160.0889,65653015.56,175);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Acrylic','Gasoline (without ethanol)',157.2,365624.0916,63984216.03,175);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Acrylic','Propane',139,323293.567,56576374.23,175);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Acrylic','Natural gas',117,272124.801,47621840.18,175);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Cotton','Coal (anthracite)',228.6,531689.9958,31901399.75,60);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Cotton','Coal (bituminous)',205.7,478427.9621,28705677.73,60);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Cotton','Coal (lignite)',215.4,500988.7362,30059324.17,60);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Cotton','Coal (subbituminous)',214.3,498430.2979,29905817.87,60);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Cotton','Diesel fuel and heating oil',161.3,375160.0889,22509605.33,60);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Cotton','Gasoline (without ethanol)',157.2,365624.0916,21937445.5,60);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Cotton','Propane',139,323293.567,19397614.02,60);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Cotton','Natural gas',117,272124.801,16327488.06,60);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Linen','Coal (anthracite)',228.6,531689.9958,5316899.958,10);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Linen','Coal (bituminous)',205.7,478427.9621,4784279.621,10);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Linen','Coal (lignite)',215.4,500988.7362,5009887.362,10);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Linen','Coal (subbituminous)',214.3,498430.2979,4984302.979,10);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Linen','Diesel fuel and heating oil',161.3,375160.0889,3751600.889,10);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Linen','Gasoline (without ethanol)',157.2,365624.0916,3656240.916,10);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Linen','Propane',139,323293.567,3232935.67,10);
+INSERT INTO mytable(Material,Power_Source,CO2emBTU,C02e1MJkg,Emissionskg,EnergyMJKg) VALUES ('Linen','Natural gas',117,272124.801,2721248.01,10);
